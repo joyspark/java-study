1ab94d1aa7f7841254a15d459a01a3b722d2dd4f
Jihye <Jihye@gmail.com>
2020-02-13 09:13:27 +0900

    [서지혜] lesson 3-2 올림

--- AlgorithmStudy/src/study/algorithm/codility/lessons/TimeComplexity.java
+++ AlgorithmStudy/src/study/algorithm/codility/lessons/TimeComplexity.java
@@ -14,17 +14,18 @@ public class TimeComplexity {
 	
 	public static void main(String[] args) {
 		// 1) Perm Missing Elem
-		int[] A = {};
-		System.out.println(solution1(A));
+		int[] A = {3,1,2,4,3};
+		//System.out.println(solution1(A));
 		// 2) Tape Equilibrium
-		
+		System.out.println(solution2(A));
 		// 3) Frog Jmp
 	}
 	
 	/**
+	 * 배열 중간에 빠진 숫자 하나를 찾는 알고리즘
 	 * 
-	 * @param A
-	 * @return
+	 * @param A => int[1~N+1] (N : 1~100,000사이), 중복되는 숫자는 없음
+	 * @return 빠진 숫자
 	 */
 	public static int solution1(int[] A) {
 		int missingElem = 0;
@@ -54,5 +55,46 @@ public class TimeComplexity {
 			}
 			return missingElem;
 		}
+	}
+	
+	
+	/**
+	 * 한 칸씩 이동하면서
+	 * 앞에서 뒤를 뺏을 때
+	 * 가장 차이가 적은 숫자를 return
+	 * 
+	 * @param A [-1,000~1,000], 길이 (2~100,000)
+	 * @return
+	 */
+	public static int solution2(int[] A) {
+		int front = 0;
+		int back = 0;
+		int min = 0;
+		int diff = 0;
+		
+		for(int i=0; i<A.length-1; i++) {
+			// 첫 루틴 계산
+			if(i==0) {
+				// 앞쪽 더하기 계산
+				front += A[0];
+				// 뒤쪽 더하기 계산
+				for(int j=1; j<A.length; j++)
+					back += A[j];
+			} else {
+				front += A[i];
+				back -= A[i];
+			}
+			// 차이 계산 
+			diff = front-back; 
+			if(diff < 0)
+				diff = diff * -1;
+			// diff 최소 값 비교
+			if(i==0)
+				min = diff;
+			else if(diff < min)
+				min = diff;
+		}
+		
+		return min;
 	}
 }
